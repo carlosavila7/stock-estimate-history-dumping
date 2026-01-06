b0eae439098784a196c00e3cfa2c4524e97a152f
Carlos Avila <carlos_ed03@outlook.com>
2026-01-06 11:18:59 -0300

    chore: set logs to be written on file

--- query_estimate_data.py
+++ query_estimate_data.py
@@ -14,6 +14,8 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
+    filename='query_estimate_data.log',
+    filemode='a',
     force=True
 )
 
