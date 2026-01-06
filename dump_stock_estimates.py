95e511c37835e2d02003991b8f399c4f23eadbc1
Carlos Avila <carlos_ed03@outlook.com>
2026-01-06 16:12:49 -0300

    chore: update file logger

--- dump_stock_estimates.py
+++ dump_stock_estimates.py
@@ -13,7 +13,7 @@ from stock_estimates_db import StockEstimatesDB
 
 log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-file_handler = logging.FileHandler('query_estimate_data.log', mode='a')
+file_handler = logging.FileHandler('data/dump_stock_estimates.log', mode='a')
 file_handler.setFormatter(log_format)
 
 console_handler = logging.StreamHandler(sys.stdout)
--- map_symbols.py
+++ map_symbols.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-file_handler = logging.FileHandler('query_estimate_data.log', mode='a')
+file_handler = logging.FileHandler('data/map_symbols.log', mode='a')
 file_handler.setFormatter(log_format)
 
 console_handler = logging.StreamHandler(sys.stdout)
